3c38d915fbfce091f9937c1de06e0355845812a3
fel1x0u <73656401+fel1x0u@users.noreply.github.com>
2021-11-01 14:49:43 -0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -f $HOME/bin/geturl 2> /dev/null
+rm -f $HOME/bin/geturl &2> /dev/null
 
-rm -f /usr/local/bin/geturl 2> /dev/null
+rm -f /usr/local/bin/geturl &2> /dev/null
 
 echo "geturl should be sucessfully be uninstalled on the system!"
